bef28a0ea442bc7db44104035fec935a0c0e832c
Ivan Fedorov <idfedorov09@gmail.com>
2023-10-29 15:40:56 +0300

    flow transfer to another library

--- src/main/java/ru/idfedorov09/telegram/bot/flow/InjectData.kt
+++
@@ -1,5 +0,0 @@
-package ru.idfedorov09.telegram.bot.flow
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION)
-annotation class InjectData
--- src/main/java/ru/idfedorov09/telegram/bot/flow/NodeType.kt
+++
@@ -1,8 +0,0 @@
-package ru.idfedorov09.telegram.bot.flow
-
-// WGNode - узел типа WAIT/GROUP
-enum class NodeType {
-    WAIT, // узел ожидания (ожидает завершения выполнения всех предков); fetcher = null
-    GROUP, // типа WAIT, но без ожидания - просто для объединения в группу; fetcher = null
-    FETCHER, // выполняющийся узел; fetcher!=null
-}
