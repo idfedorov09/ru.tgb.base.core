e1a80951fa9fc3c3237dd8d68e9ca2aabeb0262a
Ivan Fedorov <idfedorov09@gmail.com>
2023-11-07 11:34:19 +0300

    adapt to flow-lib update

--- src/main/java/ru/idfedorov09/telegram/bot/data/GlobalConstants.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/data/GlobalConstants.kt
@@ -2,7 +2,7 @@ package ru.idfedorov09.telegram.bot.data
 
 object GlobalConstants {
 
-    const val ENDPOINT_HEAL = "/is_alive"
+    const val ENDPOINT_HEALTH = "/is_alive"
 
     const val QUALIFIER_FLOW_TG_BOT = "tg_bot_flow_builder"
     const val QUALIFIER_FLOW_HEALTH_STATUS = "health_flow_builder"
