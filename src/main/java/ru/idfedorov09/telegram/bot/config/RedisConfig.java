e3f953529b2f8b0af5936176aefff55a3e348a0f
Ivan Fedorov <idfedorov09@gmail.com>
2023-11-09 22:27:59 +0300

    config settings & buildGradle edited

--- src/main/java/ru/idfedorov09/telegram/bot/config/RedisConfig.java
+++ src/main/java/ru/idfedorov09/telegram/bot/config/RedisConfig.java
@@ -6,16 +6,14 @@ import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.context.annotation.PropertySource;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 import ru.idfedorov09.telegram.bot.data.model.RedisServerData;
 import ru.idfedorov09.telegram.bot.util.RedisUtil;
 
 @Configuration
 @EnableCaching
+@PropertySource("classpath:${main-property:application}.properties")
 public class RedisConfig extends CachingConfigurerSupport {
 
     @Value("${spring.redis.host}")
