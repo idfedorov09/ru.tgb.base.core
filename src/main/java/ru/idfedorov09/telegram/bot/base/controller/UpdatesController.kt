ef64ebb6c0871dc3bf76ab49375ea49ec739c0ac
Ivan <idfedorov09@gmail.com>
2024-05-18 18:29:45 +0300

    SBOT-1967: select flow before run feature

--- src/main/java/ru/idfedorov09/telegram/bot/base/controller/UpdatesController.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/base/controller/UpdatesController.kt
@@ -2,8 +2,6 @@ package ru.idfedorov09.telegram.bot.base.controller
 
 import kotlinx.coroutines.Dispatchers
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.objects.Update
@@ -11,10 +9,7 @@ import ru.idfedorov09.telegram.bot.base.UpdatesHandler
 import ru.idfedorov09.telegram.bot.base.UpdatesSender
 import ru.idfedorov09.telegram.bot.base.data.GlobalConstants.QUALIFIER_SYSTEM_FLOW
 import ru.idfedorov09.telegram.bot.base.service.FlowBuilderService
-import ru.idfedorov09.telegram.bot.base.service.RedisService
-import ru.mephi.sno.libs.flow.belly.FlowBuilder
 import ru.mephi.sno.libs.flow.belly.FlowContext
-import java.time.format.DateTimeFormatter
 
 @Component
 class UpdatesController(
@@ -24,10 +19,6 @@ class UpdatesController(
     companion object {
         private val log = LoggerFactory.getLogger(UpdatesController::class.java)
     }
-
-    @Autowired
-    @Qualifier(QUALIFIER_SYSTEM_FLOW)
-    private lateinit var systemFlowBuilder: FlowBuilder
 
     // @Async("infinityThread") // if u need full async execution
     override fun handle(telegramBot: TelegramLongPollingBot, update: Update) {
